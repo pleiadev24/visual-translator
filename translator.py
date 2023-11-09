c9b88e4792bbe8e17f0c20f0e0c0593ef6e57d9d
Roh <69511860+pleiadev24@users.noreply.github.com>
2023-11-09 11:51:25 +0530

    Update translator.py
    Added exception handling

--- translator.py
+++ translator.py
@@ -1,3 +1,4 @@
+import cv2
 import pytesseract
 from google.cloud import translate_v2 as translate
 
@@ -7,33 +8,38 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tessera
 # Set up Google Cloud Translation API (replace 'YOUR_API_KEY' with your actual API key)
 translate_client = translate.Client(api_key='YOUR_API_KEY')
 
-import cv2
-
-# capture, process, and translate frames in real time
+# Function to capture, process, and translate frames in real time
 def translate_text_from_frame():
-    cap = cv2.VideoCapture(0)  # Open the default camera (index 0)
-
-    while True:
-        ret, frame = cap.read()  # Read a frame from the camera
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # Convert to grayscale
-
-        # Use OCR to extract text from the frame
-        extracted_text = pytesseract.image_to_string(gray)
-
-        # Translate the extracted text using Google Cloud Translation API
-        if extracted_text:
-            translated_text = translate_client.translate(extracted_text, target_language='en')  # Translate to English
-            print("Extracted Text:", extracted_text)
-            print("Translated Text:", translated_text['input'])
+    try:
+        cap = cv2.VideoCapture(0)  # Open the default camera (index 0)
         
-        # Display the original frame with extracted text
-        cv2.imshow('Real-Time Translator', frame)
+        while True:
+            ret, frame = cap.read()  # Read a frame from the camera
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # Convert the frame to grayscale
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):  # Press 'q' to exit the loop and close the window
-            break
+            # Use OCR to extract text from the frame
+            extracted_text = pytesseract.image_to_string(gray)
 
-    cap.release()  # Release the camera
-    cv2.destroyAllWindows()  # Close all OpenCV windows
+            # Translate the extracted text using Google Cloud Translation API
+            if extracted_text:
+                try:
+                    translated_text = translate_client.translate(extracted_text, target_language='en')  # Translate to English
+                    print("Extracted Text:", extracted_text)
+                    print("Translated Text:", translated_text['input'])
+                except Exception as translation_error:
+                    print("Translation Error:", translation_error)
+            
+            # Display the original frame with extracted text
+            cv2.imshow('Real-Time Translator', frame)
 
-#start real-time translation
+            if cv2.waitKey(1) & 0xFF == ord('q'):  # Press 'q' to exit the loop and close the window
+                break
+
+    except Exception as e:
+        print("An error occurred:", e)
+    finally:
+        cap.release()  # Release the camera
+        cv2.destroyAllWindows()  # Close all OpenCV windows
+
+# Call the function to start real-time translation
 translate_text_from_frame()
